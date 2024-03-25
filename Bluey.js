c6687125fd1e72a07b856a6334b2ce8225fb74d7
Luk9as <diazbrunalucas@gmail.com>
2024-03-25 20:09:28 -0300

    PUTA ESTE SI SERVIA

--- Bluey.js
+++ Bluey.js
@@ -912,7 +912,7 @@ ApplicationMain.main = function() {
 ApplicationMain.create = function(config) {
 	var app = new openfl_display_Application();
 	ManifestResources.init(config);
-	app.meta.h["build"] = "427";
+	app.meta.h["build"] = "428";
 	app.meta.h["company"] = "Yo";
 	app.meta.h["file"] = "Bluey";
 	app.meta.h["name"] = "Bluey";
@@ -3418,6 +3418,7 @@ var Main = function() {
 	}
 	flixel_FlxG.autoPause = false;
 	this.addChild(new flixel_FlxGame(this.gameWidth,this.gameHeight,this.initialState,this.zoom,this.framerate,this.framerate,this.skipSplash,this.startFullscreen));
+	this.enableFullscreen();
 };
 $hxClasses["Main"] = Main;
 Main.__name__ = "Main";
@@ -3430,6 +3431,21 @@ Main.prototype = $extend(openfl_display_Sprite.prototype,{
 	,framerate: null
 	,skipSplash: null
 	,startFullscreen: null
+	,enableFullscreen: function() {
+		var document = window.document;
+		if(document.fullscreenEnabled || Reflect.field(document,"webkitFullscreenEnabled") || Reflect.field(document,"mozFullScreenEnabled") || Reflect.field(document,"msFullscreenEnabled")) {
+			var element = window.document.getElementById("game-container");
+			if(Reflect.field(element,"requestFullscreen")) {
+				Reflect.field(element,"requestFullscreen").apply(element,[]);
+			} else if(Reflect.field(element,"webkitRequestFullscreen")) {
+				Reflect.field(element,"webkitRequestFullscreen").apply(element,[]);
+			} else if(Reflect.field(element,"mozRequestFullScreen")) {
+				Reflect.field(element,"mozRequestFullScreen").apply(element,[]);
+			} else if(Reflect.field(element,"msRequestFullscreen")) {
+				Reflect.field(element,"msRequestFullscreen").apply(element,[]);
+			}
+		}
+	}
 	,__class__: Main
 });
 var DocumentClass = function(current) {
@@ -70037,7 +70053,7 @@ var lime_utils_AssetCache = function() {
 	this.audio = new haxe_ds_StringMap();
 	this.font = new haxe_ds_StringMap();
 	this.image = new haxe_ds_StringMap();
-	this.version = 683918;
+	this.version = 626557;
 };
 $hxClasses["lime.utils.AssetCache"] = lime_utils_AssetCache;
 lime_utils_AssetCache.__name__ = "lime.utils.AssetCache";
